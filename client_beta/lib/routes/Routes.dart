5afa2a7b6e90a1c9a73eab3f005adc7f853ed9d5
NV-Phong <0h9ng03@gmail.com>
2024-10-18 18:27:22 +0700

    [CHORE] Test API

--- client_beta/lib/routes/Routes.dart
+++ client_beta/lib/routes/Routes.dart
@@ -1,15 +1,18 @@
 import 'package:client_beta/screens/Register.dart';
+import 'package:client_beta/screens/TestData.dart';
 import 'package:flutter/material.dart';
 import '../screens/Login.dart';
 
 class AppRoutes {
   static const String login = '/';
-  static const String register = '/Register';
+  static const String register = '/register';
+  static const String testdata = '/testdata';
 
   static Map<String, WidgetBuilder> get routes {
     return {
       login: (context) => Login(),
       register: (context) => RegisterPage(),
+      testdata: (context) => DataScreen()
     };
   }
 }
---
+++ client_beta/lib/screens/TestData.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class DataScreen extends StatefulWidget {
+  @override
+  _DataScreenState createState() => _DataScreenState();
+}
+
+class _DataScreenState extends State<DataScreen> {
+  List<dynamic> _data = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    final response = await http.get(Uri.parse('http://localhost:3000/user'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _data = json.decode(response.body);
+        _isLoading = false;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Data Screen'),
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _data.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_data[index]['username']),
+                  subtitle: Text(_data[index]['password']),
+                );
+              },
+            ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: DataScreen(),
+  ));
+}
