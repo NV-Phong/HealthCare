1ee7baa9c9956311a741ae7340d655380114275f
Pham Van Phuoc <phuoc3003pham@gmail.com>
2024-10-19 13:04:51 +0700

    [refactor] main.ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
+
+   // Tùy chỉnh cấu hình CORS
+   app.enableCors({
+      origin: '*', // Chỉ cho phép nguồn gốc cụ thể
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Các phương thức được phép
+      // allowedHeaders: 'Content-Type, Authorization', // Các header được phép
+      credentials: true, // Cho phép cookies hoặc các thông tin xác thực
+   });
+
    await app.listen(3000);
 }
 bootstrap();
