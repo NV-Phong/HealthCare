c9ccce3b6ece470856c75ab9b83454e5fc7a2ad6
LauHoangNguyen <hoangnguyen.lau@gmail.com>
2024-10-21 20:07:39 +0700

    [feat] Progress completion

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Get, Param, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, Req, UseGuards } from '@nestjs/common';
 import { User } from '../schema/user.schema';
 import { UserService } from './user.service';
 import { JwtAuthGuard } from 'src/configuration/jwt-auth.guard';
+import { Request } from 'express';
 
 @Controller('user')
-// @UseGuards(JwtAuthGuard)
+ @UseGuards(JwtAuthGuard)
 export class UserController {
    constructor(private readonly userService: UserService) {}
 
@@ -19,5 +20,22 @@ export class UserController {
     async updateUser(@Param('id') userId: string, @Body() updateData: Partial<User>) {
         return this.userService.updateUser(userId, updateData);
     }
+
+    @Get('profile-completion/:id')
+    async getProfileCompletion(@Param('id') id: string) {
+      const user = await this.userService.findById(id);
+        if (!user) {
+            return { message: 'User not found' };
+        }
+      const completion = await this.userService.calculateProfileCompletion(user);
+    return { completion };
+    }
+
+    @Get('profile-completion')
+   async getProfileCompletionbytoken(@Req() req) {
+     const user = req.user; // user đã được lấy từ `token` thông qua JwtAuthGuard
+     const completion = await this.userService.calculateProfileCompletion(user);
+     return { completion };
+   }
 
 }
